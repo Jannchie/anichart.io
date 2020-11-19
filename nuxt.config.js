115f476728578a92bdfc2f5bd2cdf763f6a3ad7a
jannchie <jannchie@gmail.com>
2020-11-19 16:01:09 +0900

    Update: Static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors';
 
 export default {
+  target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - anichart.io',
